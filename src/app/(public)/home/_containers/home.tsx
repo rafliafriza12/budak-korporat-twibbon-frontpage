90bfc9bfb5d355411e8dd4319e4ed9a4ddb7e3d9
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-05-04 02:37:08 +0700

    feat:add section top creator and let try

--- src/app/(public)/home/_containers/home.tsx
+++ src/app/(public)/home/_containers/home.tsx
@@ -2,6 +2,8 @@
 import { themeConfig } from '@/configs/theme.config';
 import NavLayout from '@/core/layouts/nav.layout';
 import { useTheme } from '@/core/providers/theme.provider';
+import TopCreator from '@/components/section/top-creator/TopCreator';
+import LetTry from '@/components/section/let’t try/Let-Try';
 
 export default function ContainerHome() {
   const { theme } = useTheme();
@@ -9,8 +11,9 @@ export default function ContainerHome() {
   return (
     <NavLayout>
       <main className={`container mx-auto bg-[${themeConfig[theme].primary.background}]`}>
-        <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-4xl font-bold">Home</h1>
+        <div className="flex flex-col items-center justify-center h-full">
+          <TopCreator />
+          <LetTry />
         </div>
       </main>
     </NavLayout>
