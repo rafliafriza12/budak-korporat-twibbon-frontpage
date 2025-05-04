de5cd953106a2f0a3a7134207e6c42913e0f05ea
rafliafriza12 <rafliafriza90@gmail.com>
2025-05-04 17:58:15 +0700

    fix: fixed bug in interface

--- src/components/card/top-creator/card.tsx
+++ src/components/card/top-creator/card.tsx
@@ -8,10 +8,10 @@ const CardComponentTopCreator: React.FC<CardConfigProps> = ({ data }) => {
 
   return (
     <main
-      className={`flex border-2 w-[80vw] h-[45vh] lg:h-auto lg:w-[35vh] bg-card flex-col items-center gap-[80px]  p-5`}
+      className={`flex border-2 w-[80vw] h-[45vh] lg:h-auto lg:w-[35vh] bg-card flex-col items-center gap-[50px]  p-5`}
     >
-      <div className="bg-[#D9D9D9] w-[100%] h-[20vh] relative z-0 p-5 flex justify-center">
-        <div className="bg-[#D9D9D9]  w-20 h-20 scale-1.5 border-2  absolute top-36 rounded-full border-black"></div>
+      <div className="bg-[#D9D9D9] w-[100%] h-[300px] relative z-0 p-5 flex justify-center">
+        <div className="bg-[#D9D9D9]  w-20 h-20 scale-1.5 border-2  absolute -bottom-[40px] rounded-full border-black"></div>
       </div>
       <div className="flex flex-col w-full gap-2 text-center items-center">
         <h1 className="">{data.username}</h1>
